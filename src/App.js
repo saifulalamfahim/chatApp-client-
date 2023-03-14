ebe92b9069a683087a952dda070c2a84d72dd4bb
saifulalamfahime <you@example.com>
2023-03-15 01:49:52 +0600

    client site completed

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import ChatContainer from "./components/ChatContainer";
+
+
+function App() {
+  return (
+   <div style={{backgroundColor: "#ece6dd", height:'100vh'}}>
+        <ChatContainer/>
+   </div>
+  );
+}
+
+export default App;
